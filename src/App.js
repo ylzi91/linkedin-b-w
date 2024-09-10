9e20fc11e0b74c869f93aa91cb8a7c33fa71b5e5
RiccardoMamoli <riccardomamoli2@gmail.com>
2024-09-10 10:43:31 +0200

    commit container home

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { CREATE_NEW_POST, DELETE_POST, MODIFY_POST, getCategory, getCompany, getOrModifyPost, getSearch } from './redux/actions';
 import CustomNavbar from './components/CustomNavbar';
+import Home from './components/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   const dispatch = useDispatch()
@@ -15,9 +17,16 @@ function App() {
 
   return (
     <>
-      <header className='d-flex align-items-center justify-content-center'>
-        <CustomNavbar />
-      </header>
+      <BrowserRouter>
+        <header className='d-flex align-items-center justify-content-center'>
+          <CustomNavbar />
+        </header>
+        <main className='d-flex justify-content-center'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
     </>
   );
 }
