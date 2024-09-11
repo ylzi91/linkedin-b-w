aaf984fa06cec84ac91ed60379abd48d4f828c94
SaraPavone <sara.pavone.92@gmail.com>
2024-09-11 15:46:05 +0200

    modifiche search bar

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -1,4 +1,5 @@
 import { Navbar, Nav } from "react-bootstrap";
+import { useState } from "react";
 import LinkedinIcon from "./navComp/LinkedinIcon";
 import SearchBar from "./navComp/SearchBar";
 import HomeButton from "./navComp/HomeButton";
@@ -12,6 +13,11 @@ import SearchButton from "./navComp/SearchButton";
 import Dots from "./navComp/Dots";
 
 const CustomNavbar = () => {
+  const [showButtons, setShowButtons] = useState(true);
+  const handleSearchButtonClick = () => {
+    setShowButtons(false);
+  };
+
   return (
     <div
       id="custom-navbar"
@@ -22,44 +28,57 @@ const CustomNavbar = () => {
           <Navbar.Brand>
             <LinkedinIcon />
           </Navbar.Brand>
-          <Nav.Link className="ps-1 d-none d-lg-flex underl">
+
+          <Nav.Link className="ps-1 d-flex d-lg-none underl">
+          <SearchButton onClick={handleSearchButtonClick} />
+          </Nav.Link>
+          <Nav.Link className="d-none d-lg-flex ps-1 underl">
+          <SearchBar />
+          </Nav.Link>
+          </div>
+        {showButtons ? (
+          <>
+            <div className="d-flex align-items-center justify-content-around flex-grow-1">
+              <Nav.Link className="text-light d-flex d-lg-none underl">
+               
+              </Nav.Link>
+              <Nav.Link className="underl">
+                <HomeButton />
+              </Nav.Link>
+              <Nav.Link className="underl">
+                <ReteButton />
+              </Nav.Link>
+              <Nav.Link className="underl">
+                <LavoroButton />
+              </Nav.Link>
+              <Nav.Link className="underl">
+                <MessaggiButton />
+              </Nav.Link>
+              <Nav.Link className="underl">
+                <NotificheButton />
+              </Nav.Link>
+              <Nav.Link className="me-2 d-none d-md-flex">
+                <UserDropdown />
+              </Nav.Link>
+              <Nav.Link className="d-flex d-md-none underl">
+                <Dots />
+              </Nav.Link>
+            </div>
+
+            <div className="d-flex align-items-center">
+              <Nav.Link className="d-none d-md-flex mx-2 ">
+                <CompaniesDropdown />
+              </Nav.Link>
+              <Nav.Link className="d-none d-md-flex">
+                <p className="premium">Prova Premium per 0 EUR</p>
+              </Nav.Link>
+            </div>
+          </>
+        ) : (
+          <div className="w-100 d-flex justify-content-center">
             <SearchBar />
-          </Nav.Link>
-        </div>
-        <div className="d-flex align-items-center justify-content-around flex-grow-1">
-          <Nav.Link className="text-light d-flex d-lg-none underl">
-            <SearchButton />
-          </Nav.Link>
-          <Nav.Link className="underl">
-            <HomeButton/>
-          </Nav.Link>
-          <Nav.Link className="underl">
-            <ReteButton />
-          </Nav.Link>
-          <Nav.Link className="underl">
-            <LavoroButton />
-          </Nav.Link>
-          <Nav.Link className="underl">
-            <MessaggiButton />
-          </Nav.Link>
-          <Nav.Link className="underl">
-            <NotificheButton />
-          </Nav.Link>
-          <Nav.Link className="me-2 d-none d-md-flex">
-            <UserDropdown />
-          </Nav.Link>
-          <Nav.Link className="d-flex d-md-none underl">
-            <Dots />
-          </Nav.Link>
-        </div>
-        <div className="d-flex align-items-center">
-          <Nav.Link className="d-none d-md-flex mx-2 ">
-            <CompaniesDropdown />
-          </Nav.Link>
-          <Nav.Link className="d-none d-md-flex">
-            <p className="premium">Prova Premium per 0 EUR</p>
-          </Nav.Link>
-        </div>
+          </div>
+        )}
       </Nav>
     </div>
   );
--- src/components/navComp/SearchButton.jsx
+++ src/components/navComp/SearchButton.jsx
@@ -1,85 +1,38 @@
 import React, { useState } from "react";
-import { Button, FormControl } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import { FaSearch } from "react-icons/fa";
 
 const SearchButton = () => {
- const [isSearchActive, setIsSearchActive] = useState (false);
- const [searchQuery, setSearchQuery] = useState ("");
-
-const handleToggle = () => {
-  setIsSearchActive(!isSearchActive);
-
-};
-
-const handleInputChange = (e) => {
-  setSearchQuery(e.target.value);
-};
-return (
-  <div align="end">
-    {isSearchActive ? (
-      <div className="d-flex align-items-center">
-        <FormControl
-          type="text"
-          placeholder="Cerca..."
-          value={searchQuery}
-          onChange={handleInputChange}
-          className="bg-dark text-white"
-        />
-        <Button
-          variant="link"
-          onClick={handleToggle}
-          className="text-white ms-2"
-        >
+  const [showInput, setShowInput] = useState(false);
+  const handleIconClick = () => {
+    setShowInput(true);
+  
+  };
+  return (
+    <div>
+       {!showInput && (
+       <FaSearch onClick={
+        handleIconClick} />
+      )}
+      {showInput && (
+        <div>
+          <Button className="bg-dark text-white border-0">
           <FaSearch />
-        </Button>
-      </div>
-    ) : (
-      <div className="d-flex align-items-center">
-        <Button
-          variant="link"
-          onClick={handleToggle}
-          className="d-md-none text-white bg-dark" 
-        >
-          <FaSearch />
-        </Button>
-      </div>
-    )}
-  </div>
+         <input
+           type="text"
+            placeholder="Cerca..."
+            className="bg-dark text-white"
+          />
+          </Button>
+        </div>
+       
+          
+        )}
+      
+    </div>
   );
 };
+ 
 
-export default SearchButton
 
-// import React, { Component } from 'react';
-// import ReactDOM from 'react-dom';
-// import Toggle from 'react-bootstrap-toggle';
- 
-// class Form extends Component {
-//   constructor() {
-//     super();
-//     this.state = { toggleActive: false };
-//     this.onToggle = this.onToggle.bind(this);
-//   }
- 
-//   onToggle() {
-//     this.setState({ toggleActive: !this.state.toggleActive });
-//   }
- 
-//   render() {
-//     return (
-//       <form>
-//         <SomeInput something={someProp} />
-//         .....
-//         <Toggle
-//           onClick={this.onToggle}
-//           on={<h2>ON</h2>}
-//           off={<h2>OFF</h2>}
-//           size="xs"
-//           offstyle="danger"
-//           active={this.state.toggleActive}
-//         />
-//       </form>
-//     )
-//   }
- 
-// }
+export default SearchButton;
