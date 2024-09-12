7be5311e3a8a244cc82db94220fbc79234e221dc
ylzi91 <97354085+ylzi91@users.noreply.github.com>
2024-09-12 18:13:53 +0200

    Fetch dei commenti e tutte le cose belle

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -15,6 +15,8 @@ export const GET_SEARCH = 'GET_SEARCH'
 export const GET_COMPANY = 'GET_COMPANY'
 export const GET_CATEGORY = 'GET_CATEGORY'
 export const GET_COMMENT_POST = 'GET_COMMENT_POST'
+export const WRITE_COMMENT_POST = 'WRITE_COMMENT_POST'
+export const MODIFY_COMMENT_POST = 'MODIFY_COMMENT_POST'
 
 
 // PROFILE ------------------------------------------
@@ -344,6 +346,108 @@ export const takeComments = () => {
                 const arrayObj = await response.json()
                 dispatch({
                     type: GET_COMMENT_POST,
+                    payload: arrayObj
+                })
+            }
+            else {
+                alert('Errrore Fetch')
+
+            }
+        }
+        catch (error) {
+            console.log(error)
+
+        }
+    }
+}
+export const addComment = (idPost, comment, rate = '1') => {
+    return async (dispatch, getState) => {
+        const myUrl = `https://striveschool-api.herokuapp.com/api/comments/`
+        const key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmUyYTBkZDU0M2E0YzAwMTU5MDFkZmIiLCJpYXQiOjE3MjYxMjgzNDksImV4cCI6MTcyNzMzNzk0OX0.5FrJw7Z8uGN3muC9bnpUEZlfusI8TbD7sfnwiHYJGWA'
+        try {
+            const response = await fetch(myUrl, {
+                method: 'POST',
+                headers: {
+                    Authorization: `Bearer ${key}`,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    comment: comment,
+                    rate: rate,
+                    elementId: idPost
+                })
+                
+            })
+            if (response.ok) {
+                const arrayObj = await response.json()
+                dispatch({
+                    type: WRITE_COMMENT_POST,
+                    payload: arrayObj
+                })
+            }
+            else {
+                alert('Errrore Fetch')
+
+            }
+        }
+        catch (error) {
+            console.log(error)
+
+        }
+    }
+}
+export const modifyComment = (idComment, comment) => {
+    return async (dispatch, getState) => {
+        const myUrl = `https://striveschool-api.herokuapp.com/api/comments/${idComment}`
+        const key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmUyYTBkZDU0M2E0YzAwMTU5MDFkZmIiLCJpYXQiOjE3MjYxMjgzNDksImV4cCI6MTcyNzMzNzk0OX0.5FrJw7Z8uGN3muC9bnpUEZlfusI8TbD7sfnwiHYJGWA'
+        try {
+            const response = await fetch(myUrl, {
+                method: 'PUT',
+                headers: {
+                    Authorization: `Bearer ${key}`,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    comment: comment,
+                })
+                
+            })
+            if (response.ok) {
+                const arrayObj = await response.json()
+                dispatch({
+                    type: MODIFY_COMMENT_POST,
+                    payload: arrayObj
+                })
+            }
+            else {
+                alert('Errrore Fetch')
+
+            }
+        }
+        catch (error) {
+            console.log(error)
+
+        }
+    }
+}
+export const deleteComment = (idComment) => {
+    return async (dispatch, getState) => {
+        const myUrl = `https://striveschool-api.herokuapp.com/api/comments/${idComment}`
+        const key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmUyYTBkZDU0M2E0YzAwMTU5MDFkZmIiLCJpYXQiOjE3MjYxMjgzNDksImV4cCI6MTcyNzMzNzk0OX0.5FrJw7Z8uGN3muC9bnpUEZlfusI8TbD7sfnwiHYJGWA'
+        try {
+            const response = await fetch(myUrl, {
+                method: 'DELETE',
+                headers: {
+                    Authorization: `Bearer ${key}`,
+                    'Content-Type': 'application/json'
+                },
+               
+        
+            })
+            if (response.ok) {
+                const arrayObj = await response.json()
+                dispatch({
+                    type: MODIFY_COMMENT_POST,
                     payload: arrayObj
                 })
             }
--- src/redux/reducers/commentFetch.js
+++ src/redux/reducers/commentFetch.js
@@ -1,4 +1,4 @@
-import { GET_COMMENT_POST } from "../actions"
+import { GET_COMMENT_POST, MODIFY_COMMENT_POST, WRITE_COMMENT_POST } from "../actions"
 
 
 const initialState = {
@@ -8,15 +8,29 @@ const initialState = {
 
 export const commentReducer = (state = initialState, action) => {
     switch(action.type) {
+        
+        case GET_COMMENT_POST: {
+            return {
+                ...state,
+                allComment: action.payload
+            }
+        }
+        
+        case WRITE_COMMENT_POST: {
+            return {
+                allComment: [...state.allComment, action.payload],
+                myComment: action.payload
+            }
+        }
+        case MODIFY_COMMENT_POST: {
+            return {
+                allComment: state.allComment.filter(commment => commment._id === action.payload._id),
+                myComment: action.payload
+            }
+        }
+
         default: {
             return state
         }
-
-    case GET_COMMENT_POST: {
-        return {
-            ...state,
-            allComment: [...state.allComment, action.payload]
-        }
-    }
     }
 }
