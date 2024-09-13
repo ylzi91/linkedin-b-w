91d7630013432d99e40c39135c93be91d2e83b11
Wide97 <mwidesott@gmail.com>
2024-09-13 14:22:52 +0200

    expitem spinner

--- src/components/profileComp/ExpCard.jsx
+++ src/components/profileComp/ExpCard.jsx
@@ -7,22 +7,23 @@ import { useDispatch, useSelector } from "react-redux";
 import { getExperiences, TAKE_EXP, TAKE_MY_PROFILE } from "../../redux/actions";
 import FormExp from "./FormExp";
 import { HiOutlinePencil } from "react-icons/hi";
+import SpinnerDots from "../spinners/SpinnerDots"; 
 
 const ExpCard = ({ id }) => {
   const [add, setAdd] = useState(false);
   const [exp, setExp] = useState({});
   const [expExpand, setExpExpand] = useState(2);
   const [expand, setExpand] = useState(false);
+  const [loading, setLoading] = useState(true);
   const dispatch = useDispatch();
   const experiences = useSelector((s) => s.experience.allExperiences);
   const myProfile = useSelector((s) => s.profile.myProfile);
 
   useEffect(() => {
-    if (id) dispatch(getExperiences(id, TAKE_EXP));
-  }, [id]);
-  useEffect(() => {
-    if (id) dispatch(getExperiences(id, TAKE_EXP));
-  }, [add]);
+    if (id) {
+      dispatch(getExperiences(id, TAKE_EXP)).finally(() => setLoading(false)); 
+    }
+  }, [id, add]);
 
   const close = () => {
     setAdd(false);
@@ -30,18 +31,17 @@ const ExpCard = ({ id }) => {
 
   const openForm = (exp) => {
     setExp({ ...exp })
-    setAdd(true)
-  }
-
-
-  useEffect(() => {
-    console.log(experiences);
-  }, []);
+    setAdd(true);
+  };
 
   return (
     <>
       {add ? (
         <FormExp id={id} close={close} expid={exp} add={add} />
+      ) : loading ? (
+        <div className="d-flex justify-content-center align-items-center" style={{ height: "200px" }}>
+          <SpinnerDots />
+        </div>
       ) : (
         <Card className="rounded-3 experience-section mb-4 bg-dark text-light">
           <Card.Body>
@@ -70,23 +70,27 @@ const ExpCard = ({ id }) => {
               return <ExpItem key={exp._id} exp={exp} openForm={openForm} />;
             })}
           </Card.Body>
-          {expand ? (<div className="show-all-experiences">
-            <button className="btn btn-link text-decoration-none w-100 py-3 text-secondary fw-semibold" onClick={()=>{
-              setExpand(false)
-              setExpExpand(2)
-            }}>
-              Hide experiences{" "}
-              <span className="ms-1">&larr;</span>
-            </button>
-          </div>) : (<div className="show-all-experiences">
-            <button className="btn btn-link text-decoration-none w-100 py-3 text-secondary fw-semibold" onClick={()=>{
-              setExpand(true)
-              setExpExpand(experiences.length)
-            }}>
-              Show all {experiences.length} experiences{" "}
-              <span className="ms-1">&rarr;</span>
-            </button>
-          </div>)}
+          {expand ? (
+            <div className="show-all-experiences">
+              <button className="btn btn-link text-decoration-none w-100 py-3 text-secondary fw-semibold" onClick={() => {
+                setExpand(false);
+                setExpExpand(2);
+              }}>
+                Hide experiences{" "}
+                <span className="ms-1">&larr;</span>
+              </button>
+            </div>
+          ) : (
+            <div className="show-all-experiences">
+              <button className="btn btn-link text-decoration-none w-100 py-3 text-secondary fw-semibold" onClick={() => {
+                setExpand(true);
+                setExpExpand(experiences.length);
+              }}>
+                Show all {experiences.length} experiences{" "}
+                <span className="ms-1">&rarr;</span>
+              </button>
+            </div>
+          )}
         </Card>
       )}
     </>
