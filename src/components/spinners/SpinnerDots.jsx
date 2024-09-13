98bfc8c0aaa770aadeaf68f5f9cefdff1ec49a3b
Wide97 <mwidesott@gmail.com>
2024-09-13 11:20:40 +0200

    Adding Spinners Components

---
+++ src/components/spinners/SpinnerDots.jsx
@@ -0,0 +1,15 @@
+
+import React from "react";
+import "./SpinnerDots.css"; 
+
+const SpinnerDots = () => {
+  return (
+    <div className="spinner-dots">
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+    </div>
+  );
+};
+
+export default SpinnerDots;
