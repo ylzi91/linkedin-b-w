899517b9f956a0635db221186ef20cef8dec0b7a
antoniocostantin <costantini062@gmail.com>
2024-09-13 16:16:29 +0200

    sistemato

--- src/components/searchComp/JobS.jsx
+++ src/components/searchComp/JobS.jsx
@@ -10,78 +10,73 @@ const JobS = ({ query, setCategoryQuery }) => {
   const [jobExpand, setJobExpand] = useState(5);
   const [expand, setExpand] = useState(false);
 
-  // Effettua la ricerca dei lavori al caricamento iniziale del componente
+
   useEffect(() => {
     dispatch(getSearch(query));
-    if (allJobs.length > 0) {
-      setCategoryQuery(allJobs[0]?.category);
-    }
-  }, [dispatch, query, allJobs, setCategoryQuery]);
-
-  // Funzione per controllare se ci sono risultati
-  const filteredJobs = allJobs;
+    setCategoryQuery(allJobs[0]?.category);
+  }, [allJobs]);
 
   return (
-    <Card bg="dark" className="text-white rounded-3 text-wrap mb-2" id="Jobs">
-      {filteredJobs.length > 0 ? (
-        <>
-          <Card.Body>
-            <Card.Title className="ms-3 py-3">
-              Offerte di lavoro che potrebbero interessarti
-            </Card.Title>
-            <ListGroup className="p-0 m-0 border-0 border-bottom border-secondary pb-0 rounded-0">
-              {filteredJobs.slice(0, jobExpand).map((job, i) => (
-                <ListGroup.Item
-                  key={i}
-                  className="bg-dark px-2 py-3 d-flex align-items-center pb-2 border-0 pb-0 justify-content-between mx-4 align-content-center"
+    <>
+      <Card
+        bg="dark"
+        className="text-white rounded-3 text-wrap mb-2 "
+        id="Jobs"
+      >
+        <Card.Title className="ms-3 py-3">
+          Offerte di lavoro che potrebbero interessarti
+        </Card.Title>
+        <ListGroup className="p-0 m-0 border-0 border-bottom border-secondary pb-0 rounded-0">
+          {allJobs.slice(0, jobExpand).map((job, i) => {
+            return (
+              <ListGroup.Item
+                key={i}
+                className="bg-dark pt-2 d-flex pb-2 border-0 border-secondary border-bottom pb-0 justify-content-between mx-4 align-content-center"
+              >
+                <div className="text-white flex-column w-100 me-0 ps-1">
+                  <p className="mb-1 fw-bold">{job.title}</p>
+                  <p className="py-1 text-secondary">{job.company}</p>
+                  <p className="py-1 text-secondary">{job.location}</p>
+                </div>
+                <Button
+                  variant="link"
+                  className="text-white p-0"
+                  aria-label="Apply for job"
                 >
-                  <div className="text-white flex-column w-100 me-0 ps-1">
-                    <p className="fw-bold">{job.title}</p>
-                  </div>
-                  <Button
-                    variant="link"
-                    className="text-white p-0"
-                    aria-label="Apply for job"
-                  >
-                    <IoMdBriefcase size={20} />
-                  </Button>
-                </ListGroup.Item>
-              ))}
-            </ListGroup>
-            {/* Bottone per espandere o nascondere i lavori */}
-            {expand ? (
-              <div className="show-all-jobs">
-                <button
-                  className="btn btn-link text-decoration-none w-100 py-3 text-secondary fw-semibold"
-                  onClick={() => {
-                    setExpand(false);
-                    setJobExpand(5); // Riduci la lista a 5 lavori
-                  }}
-                >
-                  Hide jobs <span className="ms-1">&larr;</span>
-                </button>
-              </div>
-            ) : (
-              <div className="show-all-jobs">
-                <button
-                  className="btn btn-link text-decoration-none w-100 py-3 text-secondary fw-semibold"
-                  onClick={() => {
-                    setExpand(true);
-                    setJobExpand(filteredJobs.length); // Espandi la lista per mostrare tutti i lavori
-                  }}
-                >
-                  Show all {filteredJobs.length} jobs <span className="ms-1">&rarr;</span>
-                </button>
-              </div>
-            )}
-          </Card.Body>
-        </>
-      ) : (
-        <div className="d-flex align-items-center px-3 text-secondary" style={{ height: "70px" }}>
-          <p>Nessun annuncio trovato.</p>
-        </div>
-      )}
-    </Card>
+                  <IoMdBriefcase size={20} />
+                </Button>
+              </ListGroup.Item>
+            );
+          })}
+        </ListGroup>
+        {expand ? (
+          <div className="show-all-jobs">
+            <button
+              className="btn btn-link text-decoration-none w-100 py-3 text-secondary fw-semibold"
+              onClick={() => {
+                setExpand(false);
+                setJobExpand(5);
+              }}
+            >
+              Hide jobs <span className="ms-1">&larr;</span>
+            </button>
+          </div>
+        ) : (
+          <div className="show-all-jobs">
+            <button
+              className="btn btn-link text-decoration-none w-100 py-3 text-secondary fw-semibold"
+              onClick={() => {
+                setExpand(true);
+                setJobExpand(allJobs.length);
+              }}
+            >
+              Show all {allJobs.length} jobs{" "}
+              <span className="ms-1">&rarr;</span>
+            </button>
+          </div>
+        )}
+      </Card>
+    </>
   );
 };
 
--- src/components/searchComp/PostItemS.jsx
+++ src/components/searchComp/PostItemS.jsx
@@ -1,16 +1,23 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import { useEffect, useState } from "react";
-import { Row, Col, Container, Button, Form } from "react-bootstrap";
+import { Row, Col, Container, Button, Form, Dropdown } from "react-bootstrap";
 import { BiRepost } from "react-icons/bi";
 import { BsThreeDots } from "react-icons/bs";
-import { FaPen, FaTrash, FaTrashAlt } from "react-icons/fa";
+import { FaEllipsisH, FaPen, FaTrash, FaTrashAlt } from "react-icons/fa";
 import { FaRegThumbsUp } from "react-icons/fa6";
 import { GoComment } from "react-icons/go";
 import { IoIosSend } from "react-icons/io";
 import { IoPencilOutline } from "react-icons/io5";
 import { RxCross2 } from "react-icons/rx";
 import { useDispatch, useSelector } from "react-redux";
-import { addComment, DELETE_POST, deleteComment, getOrModifyPost, modifyComment, takeComments } from "../../redux/actions";
+import {
+  addComment,
+  DELETE_POST,
+  deleteComment,
+  getOrModifyPost,
+  modifyComment,
+  takeComments,
+} from "../../redux/actions";
 import { useNavigate } from "react-router-dom";
 
 const PostItemS = ({ post }) => {
@@ -22,19 +29,18 @@ const PostItemS = ({ post }) => {
   });
   const [myClick, setMyClick] = useState("");
   const [modify, setModify] = useState("");
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const [writeComment, setWriteComment] = useState("");
   const [showComment, setShowComment] = useState("");
   const comments = useSelector((store) => store.comment.allComment);
   const profiles = useSelector((store) => store.profile.allProfiles);
   const myProfile = useSelector((store) => store.profile.myProfile);
   const getProfileNameSurname = (u) => {
-      const profile = profiles.find((profile) => profile.username === u);
+    const profile = profiles.find((profile) => profile.username === u);
 
-      console.log("Profile", profile);
-      return profile;
-    
+    console.log("Profile", profile);
+    return profile;
   };
 
   const timeAgo = (timestamp) => {
@@ -80,7 +86,6 @@ const PostItemS = ({ post }) => {
     }
   };
 
-
   useEffect(() => {
     const user = { ...getProfileNameSurname(post.username) };
     console.log("user", user);
@@ -107,46 +112,58 @@ const PostItemS = ({ post }) => {
     <Row className="experience-item mb-4 gutter" id="Post">
       <Col>
         <div className="card-create px-3 py-3 rounded-0" key={post._id}>
-          {postProf.name && <div className="body-input mb-3">
-            {postProf.img && (
-              <div className="post-img clickable" onClick={()=> navigate(`/profile/${post.user._id}`)}>
-                <img
-                  className="rounded-circle profileo"
-                  src={postProf.img}
-                  alt={post.username}
-                />
-              </div>
-            )}
-            <div className="user-post w-50 h-100 ">
-              <div className="clickable" onClick={()=> navigate(`/profile/${post.user._id}`)}>
-                  {postProf.name} {postProf.surname} </div> <div>{postProf.username}
-              </div>
-              <div className="text-secondary py-1">{timeAgo(post.createdAt)}</div>
-            </div>
-            <div className="edit-icon d-flex align-items-center">
-              <BsThreeDots className="me-3" />
-              {myProfile.username === post.username ? (
-                <RxCross2 />
-              ) : (
-                <RxCross2 />
-              )}
-            </div>
-          </div>}
-          <div className="text-light py-3 px-3 border-bottom-custom d-flex justify-content-between align-items-center text-wrap">
-            {post.text}<div
-                  className=" text-end clickable text-nowrap"
-                  style={{ fontSize: "0.8em" }}
-                  onClick={() =>
-                    showComment !== post._id
-                      ? setShowComment(post._id)
-                      : setShowComment("")
-                  }
+          {postProf.name && (
+            <div className="body-input mb-3">
+              {postProf.img && (
+                <div
+                  className="post-img clickable"
+                  onClick={() => navigate(`/profile/${post.user._id}`)}
                 >
-                  {comments.filter((comment) => comment.elementId === post._id)
-                    .length > 0 &&
-                    comments.filter((comment) => comment.elementId === post._id)
-                      .length + " commenti"}
+                  <img
+                    className="rounded-circle profileo"
+                    src={postProf.img}
+                    alt={post.username}
+                  />
                 </div>
+              )}
+              <div className="user-post w-50 h-100 ">
+                <div
+                  className="clickable"
+                  onClick={() => navigate(`/profile/${post.user._id}`)}
+                >
+                  {postProf.name} {postProf.surname}{" "}
+                </div>{" "}
+                <div>{postProf.username}</div>
+                <div className="text-secondary py-1">
+                  {timeAgo(post.createdAt)}
+                </div>
+              </div>
+              <div className="edit-icon d-flex align-items-center">
+                <BsThreeDots className="me-3" />
+                {myProfile.username === post.username ? (
+                  <RxCross2 />
+                ) : (
+                  <RxCross2 />
+                )}
+              </div>
+            </div>
+          )}
+          <div className="text-light py-3 px-3 border-bottom-custom d-flex justify-content-between align-items-center text-wrap">
+            {post.text}
+            <div
+              className=" text-end clickable text-nowrap"
+              style={{ fontSize: "0.8em" }}
+              onClick={() =>
+                showComment !== post._id
+                  ? setShowComment(post._id)
+                  : setShowComment("")
+              }
+            >
+              {comments.filter((comment) => comment.elementId === post._id)
+                .length > 0 &&
+                comments.filter((comment) => comment.elementId === post._id)
+                  .length + " commenti"}
+            </div>
           </div>
           <Container className="p-0">
             <Row className="pt-1">
@@ -165,10 +182,9 @@ const PostItemS = ({ post }) => {
                 md={3}
                 lg={3}
                 xl={3}
-                className="button-media text-light d-flex  justify-content-center align-items-center p-2" onClick={() => {
-                  myClick !== post._id
-                    ? setMyClick(post._id)
-                    : setMyClick("");
+                className="button-media text-light d-flex  justify-content-center align-items-center p-2"
+                onClick={() => {
+                  myClick !== post._id ? setMyClick(post._id) : setMyClick("");
                   setWriteComment("");
                   setModify("");
                 }}
@@ -198,88 +214,109 @@ const PostItemS = ({ post }) => {
               </Col>
             </Row>
             {myClick === post._id && (
-                  <Row className=" align-items-center mt-2">
-                    <Col xs={2}>
-                      <div className="post-img">
-                        <img
-                          src={myProfile.image}
-                          alt="profile-image"
-                          className="rounded-pill"
-                        />
-                      </div>
-                    </Col>
-                    <Col className=" text-light" xs={10}>
-                      <Form
-                        onSubmit={(e) => {
-                          e.preventDefault();
-                          if (modify !== "") {
-                            dispatch(modifyComment(modify, writeComment));
-                            setModify("");
-                            setTimeout(() => {
-                              dispatch(takeComments());
-                            }, 500);
-                          } else {
-                            dispatch(addComment(post._id, writeComment));
-                            setModify("");
-                            setShowComment(post._id);
-                          }
-                          setWriteComment("");
-                        }}
-                      >
-                        <Form.Control
-                          value={writeComment}
-                          style={{ fontSize: "0.9em" }}
-                          className=" bg-dark rounded-5 text-light"
-                          placeholder="Scrivi commento..."
-                          type="text"
-                          onChange={(e) => {
-                            setWriteComment(e.target.value);
-                          }}
-                        />
-                      </Form>
-                    </Col>
-                  </Row>
-                )}
+              <Row className=" align-items-center mt-2">
+                <Col xs={2}>
+                  <div className="post-img">
+                    <img
+                      src={myProfile.image}
+                      alt="profile-image"
+                      className="rounded-pill"
+                    />
+                  </div>
+                </Col>
+                <Col className=" text-light" xs={10}>
+                  <Form
+                    onSubmit={(e) => {
+                      e.preventDefault();
+                      if (modify !== "") {
+                        dispatch(modifyComment(modify, writeComment));
+                        setModify("");
+                        setTimeout(() => {
+                          dispatch(takeComments());
+                        }, 500);
+                      } else {
+                        dispatch(addComment(post._id, writeComment));
+                        setModify("");
+                        setShowComment(post._id);
+                      }
+                      setWriteComment("");
+                    }}
+                  >
+                    <Form.Control
+                      value={writeComment}
+                      style={{ fontSize: "0.9em" }}
+                      className=" bg-dark rounded-5 text-light"
+                      placeholder="Scrivi commento..."
+                      type="text"
+                      onChange={(e) => {
+                        setWriteComment(e.target.value);
+                      }}
+                    />
+                  </Form>
+                </Col>
+              </Row>
+            )}
 
             {showComment === post._id &&
-                  comments
-                    .filter((comment) => comment.elementId === post._id)
-                    .map((cacca) => {
-                      return (
-                        <Row className="mt-2 ">
-                          <Col xs = {2} className=" p-2">
-                            <div className="post-img">
-                              <img
-                                className="rounded-circle"
-                                src={getProfileFromComment(cacca.author).image}
-
-                              />
-                            </div>
-                          </Col>
-                          <Col className=" text-light bg-secondary p-2 rounded-2 align-items-center" xs={10}>
-                          <p className=" d-flex justify-content-between mb-2"> 
-                                <span className=" fw-bold clickable" onClick={(e) => {
-                                  e.preventDefault()
-                                  if(cacca.author === 'yuri@lenzi.com'){
-                                    navigate('/myprofile')
-                                  }
-                                  else if (!getProfileFromComment(cacca.author)?._id){
-                                    alert('Non posso andare alla pagina')
-                                  }
-                                  else {
-                                    navigate(navigate(`/profile/${getProfileFromComment(cacca.author)._id}`))
-                                  }
-                                }}>{getProfileFromComment(cacca.author)?.name} {getProfileFromComment(cacca.author)?.surname}</span>
-                                <span>{timeAgo(cacca.createdAt)}</span>
-                          </p>
-                            <p className="text-light d-flex justify-content-between align-items-center">
-                              <span>
-                                {cacca.comment}
-                              </span>
-                              <span>
-                                <Button
-                                  className="rounded-start-2 me-1"
-                                  variant="outline-dark"
+              comments
+                .filter((comment) => comment.elementId === post._id)
+                .map((cacca) => {
+                  return (
+                    <Row className="mt-2 customComment" key={cacca._id}>
+                      <Col
+                        xs={2}
+                        className="d-flex align-items-center justify-content-center"
+                      >
+                        <div className="post-img m-0">
+                          <img
+                            className="rounded-circle"
+                            src={getProfileFromComment(cacca.author).image}
+                            alt="foto"
+                          />
+                        </div>
+                      </Col>
+                      <Col
+                        className="text-light p-2 rounded-2 align-items-center"
+                        xs={10}
+                      >
+                        <p className="d-flex justify-content-between">
+                          <span
+                            className="fw-bold clickable"
+                            onClick={(e) => {
+                              e.preventDefault();
+                              if (cacca.author === "yuri@lenzi.com") {
+                                navigate("/myprofile");
+                              } else if (
+                                !getProfileFromComment(cacca.author)?._id
+                              ) {
+                                alert("Non posso andare alla pagina");
+                              } else {
+                                navigate(
+                                  `/profile/${
+                                    getProfileFromComment(cacca.author)._id
+                                  }`
+                                );
+                              }
+                            }}
+                          >
+                            {getProfileFromComment(cacca.author)?.name}{" "}
+                            {getProfileFromComment(cacca.author)?.surname}
+                          </span>
+                          <div className="d-flex align-items-center">
+                            <span className="pe-2">
+                              {timeAgo(cacca.createdAt)}
+                            </span>
+                            <Dropdown align="end">
+                              <Dropdown.Toggle
+                                noCaret
+                                className="p-0 no-caret text-light bg-transparent border-0"
+                                id="dropdown-basic"
+                              >
+                                <FaEllipsisH />
+                              </Dropdown.Toggle>
+                              <Dropdown.Menu className="border border-secondary user-dropdown">
+                                <Dropdown.Item
+                                  as="button"
                                   onClick={(e) => {
                                     e.preventDefault();
                                     setMyClick(post._id);
@@ -287,11 +324,13 @@ const PostItemS = ({ post }) => {
                                     setModify(cacca._id);
                                   }}
                                 >
-                                  <FaPen />
-                                </Button>
-                                <Button
-                                  className="rounded-end-2"
-                                  variant="outline-dark"
+                                  <div className="d-flex align-items-center justify-content-center py-3">
+                                    <FaPen className="text-light me-2" />
+                                    <p className="text-light px-1">Modifica</p>
+                                  </div>
+                                </Dropdown.Item>
+                                <Dropdown.Item
+                                  as="button"
                                   onClick={() => {
                                     dispatch(deleteComment(cacca._id));
                                     setTimeout(() => {
@@ -299,15 +338,22 @@ const PostItemS = ({ post }) => {
                                     }, 1000);
                                   }}
                                 >
-                                  <FaTrash />
-                                </Button>
-                              </span>{" "}
-                            </p>
-                          </Col>
-                        </Row>
-                      );
-                    })}
-
+                                  <div className="d-flex align-items-center justify-content-center py-3">
+                                    <FaTrash className="me-2 text-light" />
+                                    <p className="text-light px-1">Elimina</p>
+                                  </div>
+                                </Dropdown.Item>
+                              </Dropdown.Menu>
+                            </Dropdown>
+                          </div>
+                        </p>
+                        <div className="text-light d-flex justify-content-between align-items-center">
+                          <span>{cacca.comment}</span>
+                        </div>
+                      </Col>
+                    </Row>
+                  );
+                })}
           </Container>
         </div>
       </Col>
